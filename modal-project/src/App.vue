f06223d86cd6a9d43b84d9d79b00aa0bf7a76ed7
Robert-Sobolewski <84735293+Robert-Sobolewski@users.noreply.github.com>
2022-07-06 13:49:08 +0200

    added modal project

---
+++ modal-project/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+ <h2>{{title}}</h2>
+ <h4>Name: {{name}}</h4>
+ <input type="text" v-model="name" name="" id="">
+ <button @click="handleClick">Click</button>
+ <Modal v-show="{visible:modal}"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+import Modal from './components/Modal.vue'
+
+export default defineComponent({
+  name: 'App',
+  data: ()=>{
+    return {
+      modal: true,
+      title: 'Hello World',
+      name: ""
+    }
+  },
+  components: {
+    HelloWorld,
+    Modal
+  },
+  methods:{
+    handleClick: function(){
+     console.log("name=", this.name);
+     
+    }
+  }
+});
+</script>
+
+<style lang="scss">
+*{
+  margin: 0;
+   padding: 0;
+    box-sizing: border-box;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+  position: relative;
+}
+</style>
---
+++ modal-project/src/components/Modal.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="modal-bg">
+        <div class="modal-body">
+            <button>Close</button>
+            <h3>modal title</h3>
+            <p>modal body Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora, neque.</p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup () {
+        
+
+        return {}
+    }
+})
+</script>
+
+<style scoped lang="scss">
+.modal-bg {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 100vw;
+    background: rgba(54, 53, 53, 0.4);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 100;
+
+    .modal-body {
+        border: solid 2px black;
+        background: #fff;
+        padding: 1em;
+        border-radius: 1em;
+    }
+}
+
+</style>
